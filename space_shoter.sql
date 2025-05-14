58515b3236abd0a0d736ab833d8ee4115b2bcdf7
Berina Majdancic <berina.majdancic.2019@gmail.com>
2025-05-14 15:41:16 +0200

    Add initial database setup with player and highscore tables

---
+++ space_shoter.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE space_shooter;
+
+USE space_shooter;
+
+CREATE TABLE IF NOT EXISTS player(
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(13) NOT NULL,
+    Password VARCHAR(64) NOT NULL,
+    Salt VARCHAR(32) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS highscore(
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Player_ID INT,
+    highscore INT NOT NULL,
+    Username VARCHAR(13),
+    FOREIGN KEY (Player_ID) REFERENCES player(ID)
+);
