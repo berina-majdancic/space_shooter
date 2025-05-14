44a7a98c0939dcddf3247ef943db0618565628b2
Berina Majdancic <berina.majdancic.2019@gmail.com>
2025-05-14 15:48:40 +0200

    Add exports and opens for db package in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,11 @@ module io.github.berinamajdancic {
     requires java.sql;
 
     opens io.github.berinamajdancic to javafx.fxml;
-
     opens io.github.berinamajdancic.controllers to javafx.fxml;
+    opens io.github.berinamajdancic.db to javafx.fxml;
 
     exports io.github.berinamajdancic;
     exports io.github.berinamajdancic.controllers to javafx.fxml;
+    exports io.github.berinamajdancic.db;
+
 }
