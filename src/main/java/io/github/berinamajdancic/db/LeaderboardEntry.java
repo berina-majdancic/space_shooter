ca4955b7f282a7f606d7a83fe0819ca86efdaf0d
Berina Majdancic <berina.majdancic.2019@gmail.com>
2025-05-14 15:48:25 +0200

    Fix LeaderboardEntry to use JavaFX properties

--- src/main/java/io/github/berinamajdancic/db/LeaderboardEntry.java
+++ src/main/java/io/github/berinamajdancic/db/LeaderboardEntry.java
@@ -1,25 +1,40 @@
 package io.github.berinamajdancic.db;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class LeaderboardEntry {
-    private String username;
-    private int score;
-    private int rank;
+    private SimpleStringProperty username;
+    private SimpleIntegerProperty score;
+    private SimpleIntegerProperty rank;
 
     LeaderboardEntry(String username, int score, int rank) {
-        this.username = username;
-        this.score = score;
-        this.rank = rank;
+        this.rank = new SimpleIntegerProperty(rank);
+        this.username = new SimpleStringProperty(username);
+        this.score = new SimpleIntegerProperty(score);
     }
 
     public int getRank() {
-        return rank;
+        return rank.get();
     }
 
     public String getUsername() {
-        return username;
+        return username.get();
     }
 
     public int getScore() {
+        return score.get();
+    }
+
+    public SimpleIntegerProperty rankProperty() {
+        return rank;
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return username;
+    }
+
+    public SimpleIntegerProperty scoreProperty() {
         return score;
     }
 }
